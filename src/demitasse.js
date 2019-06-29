bb4acdd9c4099706acf8c83bb7fbd698682c4c4d
Corey Sharrah <corey@eviltreehouse.com>
2019-06-29 12:10:43 -0700

    Iterating core assertion function -- adding option support

--- src/demitasse.js
+++ src/demitasse.js
@@ -1,31 +1,86 @@
 'use strict';
 const assert = require('simple-assert');
 const _ = require('lodash');
+const mustache = require('mustache');
+
+// HTML escaping off
+mustache.escape = (v) => v;
+
+const DEFAULT_ERR_TEMPLATE = 
+	'[Test #{{ id }}] Expected: {{ expected }}, Actual: {{ actual }}';
+
+const VERBOSE_ERR_TEMPLATE = 
+	'[Test #{{ id }}][{{ params }}] Expected: {{ expected }}, Actual: {{ actual }}';
+
+const DEFAULT_OPTS = {
+	'async': false,
+	'template': null,
+	'persist': false,
+	'verbose': false
+};
 
 /**
  * @param {function(): any[][]} dataProviderFunction
  * @param {function(...): any} testFunction
- * @return {Promise<boolean>}
+ * @param {Object.<string,any>} [opts]
+ * @return {void}
  */
-function Demitasse(dataProviderFunction, testFunction) {
+function Demitasse(dataProviderFunction, testFunction, opts) {
+	if (! opts || typeof opts !== 'object') opts = {};
+	else opts = _.cloneDeep(opts);
+
+	opts = Object.assign({}, DEFAULT_OPTS, opts);
+
+	if (! opts.template) {
+		opts.template = opts.verbose ? VERBOSE_ERR_TEMPLATE : DEFAULT_ERR_TEMPLATE;
+	}
+
 	if (typeof dataProviderFunction !== 'function') throwUsage();
 	if (typeof testFunction !== 'function') throwUsage();
 
-	var data = dataProviderFunction();
+	let data = dataProviderFunction();
 	if (! Array.isArray(data)) throwErr(`Result of ${dataProviderFunction.name} is non-array`);
 
+	let failures = [];
 	for (let idx in data) {
 		if (! Array.isArray(data[idx])) throwErr(`Row ${idx} is non-array: ${data[idx]}`);
-		var row = _.cloneDeep(data[idx]);
-		var expectedResult = row.shift();
-		var actualResult = testFunction.apply(this, row);
+		let row = _.cloneDeep(data[idx]);
+		let expectedResult = row.shift();
+		let actualResult = testFunction.apply(this, row);
+
 		if (! _.isEqual(expectedResult, actualResult)) {
-			throwErr(`[Row ${idx}] Expected: ${expectedResult.toString()}, Actual: ${actualResult.toString()}`);
+			let view = {
+				'id':       idx,
+				'params':   () => row.map(v => v.toString()).join(", "),
+				'expected': () => expectedResult.toString(),
+				'actual':   () => actualResult.toString()
+			};
+		
+			// throwErr(`[Test #${idx}] Expected: ${expectedResult.toString()}, Actual: ${actualResult.toString()}`);
+			failures.push(mustache.render(opts['template'], view));
+			if (!opts['persist']) break;
 		}
 	}
+
+	if (failures.length > 0) throwErr(failures.join('\n'));
 }
 
+/**
+ * @param {function(): any[][]} dataProviderFunction
+ * @param {function(...): any} testFunction
+ * @param {Object.<string,any>} [opts]
+ * @return {Promise<boolean>}
+ * @todo
+ */
+Demitasse.async = function(dataProviderFunction, testFunction, opts) {
+	if (! opts || typeof opts !== 'object') opts = {};
+	else opts = _.cloneDeep(opts);
+	opts['async'] = true;
+
+	return Demitasse(dataProviderFunction, testFunction, opts);
+};
+
 function throwErr(msg) { throw new Error(msg); }
-function throwUsage() { throw new Error("usage: itAlways(function, function)"); }
+function throwUsage() { throw new Error("usage: testAll(function, function)"); }
 
 module.exports = Demitasse;
--- test/00-baseline.test.js
+++ test/00-baseline.test.js
@@ -7,14 +7,51 @@ describe("Baseline", () => {
 		assert(typeof dmt === 'function');
 	});
 
-	context("Bootstrap Logic", () => {
-		const dmt = require('../index');
+	context("Usage", () => {
+		const testAll = require('../index');
 
-		it("Adding Machine", () => {
+		it("Blank", () => {
+			var threw = null;
+			try {
+				testAll();
+			} catch(e) {
+				threw = e.message;
+			}
+
+			assert(threw && threw.match(/^usage\:/));
+		});
+
+		it("Bad data provider", () => {
+			var threw = null;
+			try {
+				testAll([[1,2,1],[2,1,2]], function(){});
+			} catch(e) {
+				threw = e.message;
+			}
+
+			assert(threw && threw.match(/^usage\:/));
+		});
+
+		it("Bad tester", () => {
+			var threw = null;
+			try {
+				testAll(function(){}, true);
+			} catch(e) {
+				threw = e.message;
+			}
+
+			assert(threw && threw.match(/^usage\:/));
+		});
+	});
+
+	context("Bootstrap Logic", () => {
+		const testAll = require('../index');
+
+		it("Adding Machine = SUCCESS", () => {
 			const data = () => {
 				var tests = [];
-				
-				// Make some tests :)
+
+				// Dynamically make some tests
 				for (let i = 0; i < 1000; i++) {
 					var a = Math.floor(Math.random() * 1000) + 1;
 					var b = Math.floor(Math.random() * 1000) + 1;
@@ -25,7 +62,25 @@ describe("Baseline", () => {
 				return tests;
 			}
 
-			dmt(data, (v1, v2) => v1 + v2);
+			testAll(data, (v1, v2) => v1 + v2);
+		});
+
+		it("Bad result = FAIL", () => {
+			const data = [
+				[2, 1, 1],
+				[3, 1, 1],
+				[5, 2, 3]
+			];
+
+			var threw = null;
+			try {
+				testAll(() => data, (v1, v2) => v1 + v2);
+			} catch(e) {
+				threw = e;
+			}
+
+			assert(threw !== null);
+			assert(threw.message === '[Test #1] Expected: 3, Actual: 2', threw.message);
 		});
 	});
 });
